555ac39001ca27d7e6a093a9c8972fca553e6f03
francisnatty <fnathaniel929@gmail.com>
2024-08-09 12:35:07 +0100

    update

--- lib/src/widgets/calendar_header.dart
+++ lib/src/widgets/calendar_header.dart
@@ -7,7 +7,7 @@ import '../shared/utils.dart' show CalendarFormat, DayBuilder;
 import 'custom_icon_button.dart';
 import 'format_button.dart';
 
-class CalendarHeader extends StatelessWidget {
+class CalendarHeader extends StatefulWidget {
   final dynamic locale;
   final DateTime focusedMonth;
   final CalendarFormat calendarFormat;
@@ -36,135 +36,136 @@ class CalendarHeader extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _CalendarHeaderState createState() => _CalendarHeaderState();
+}
+
+class _CalendarHeaderState extends State<CalendarHeader> {
+  String? _selectedMonth;
+  final GlobalKey _textKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    final monthFormatter = DateFormat('MMMM', widget.locale);
+    _selectedMonth = monthFormatter.format(widget.focusedMonth);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final text = headerStyle.titleTextFormatter?.call(focusedMonth, locale) ??
-        DateFormat.yMMMM(locale).format(focusedMonth);
+    final text = widget.headerStyle.titleTextFormatter
+            ?.call(widget.focusedMonth, widget.locale) ??
+        DateFormat.yMMMM(widget.locale).format(widget.focusedMonth);
 
-    final monthFormatter = DateFormat('MMMM', locale);
-    final yearFormatter = DateFormat('yyyy', locale);
+    final monthFormatter = DateFormat('MMMM', widget.locale);
+    final yearFormatter = DateFormat('yyyy', widget.locale);
 
-    final formattedMonth = monthFormatter.format(focusedMonth);
-    final formattedYear = yearFormatter.format(focusedMonth);
+    final formattedMonth = monthFormatter.format(widget.focusedMonth);
+    final formattedYear = yearFormatter.format(widget.focusedMonth);
 
     List<String> getMonthNames(String locale) {
       final DateFormat monthFormatter = DateFormat('MMMM', locale);
-
       return List.generate(12, (index) {
         DateTime date = DateTime(2024, index + 1);
         return monthFormatter.format(date);
       });
     }
 
-    return Container(
-      decoration: headerStyle.decoration,
-      margin: headerStyle.headerMargin,
-      padding: headerStyle.headerPadding,
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          if (headerStyle.leftChevronVisible)
-            CustomIconButton(
-              icon: headerStyle.leftChevronIcon,
-              onTap: onLeftChevronTap,
-              margin: headerStyle.leftChevronMargin,
-              padding: headerStyle.leftChevronPadding,
-            ),
-          Expanded(
-            child: headerTitleBuilder?.call(context, focusedMonth) ??
-                GestureDetector(
-                  onTap: () {
-                    print(text);
-                  },
-                  onLongPress: onHeaderLongPress,
-                  child: Row(
-                    children: [
-                      InkWell(
-                        onTap: () async {
-                          var months = await getMonthNames('en_US');
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: Text('Select Month'),
-                                content: Container(
-                                  width: double.maxFinite,
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: months
-                                        .map((month) => Padding(
-                                              padding:
-                                                  const EdgeInsets.symmetric(
-                                                      vertical: 4.0),
-                                              child: Text(
-                                                month,
-                                                style:
-                                                    headerStyle.titleTextStyle,
-                                                textAlign: TextAlign.start,
-                                              ),
-                                            ))
-                                        .toList(),
-                                  ),
-                                ),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text('Close'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        },
-                        child: Row(
-                          children: [
-                            Text(
-                              formattedMonth,
-                              style: headerStyle.titleTextStyle,
-                              textAlign: headerStyle.titleCentered
-                                  ? TextAlign.center
-                                  : TextAlign.start,
-                            ),
-                            SizedBox(width: 5),
-                            Text(
-                              formattedYear,
-                              style: headerStyle.titleTextStyle,
-                              textAlign: headerStyle.titleCentered
-                                  ? TextAlign.center
-                                  : TextAlign.start,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-          ),
-          if (headerStyle.formatButtonVisible &&
-              availableCalendarFormats.length > 1)
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: FormatButton(
-                onTap: onFormatButtonTap,
-                availableCalendarFormats: availableCalendarFormats,
-                calendarFormat: calendarFormat,
-                decoration: headerStyle.formatButtonDecoration,
-                padding: headerStyle.formatButtonPadding,
-                textStyle: headerStyle.formatButtonTextStyle,
-                showsNextFormat: headerStyle.formatButtonShowsNext,
+    return Theme(
+      data: ThemeData(cardColor: Colors.white),
+      child: Container(
+        decoration: widget.headerStyle.decoration,
+        margin: widget.headerStyle.headerMargin,
+        padding: widget.headerStyle.headerPadding,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            if (widget.headerStyle.leftChevronVisible)
+              CustomIconButton(
+                icon: widget.headerStyle.leftChevronIcon,
+                onTap: widget.onLeftChevronTap,
+                margin: widget.headerStyle.leftChevronMargin,
+                padding: widget.headerStyle.leftChevronPadding,
               ),
+            Expanded(
+              child: widget.headerTitleBuilder
+                      ?.call(context, widget.focusedMonth) ??
+                  GestureDetector(
+                    onTap: () {
+                      print(text);
+                    },
+                    onLongPress: widget.onHeaderLongPress,
+                    child: Row(
+                      children: [
+                        InkWell(
+                          key: _textKey,
+                          onTap: () {
+                            final RenderBox renderBox = _textKey.currentContext!
+                                .findRenderObject() as RenderBox;
+
+                            final offset = renderBox
+                                .localToGlobal(Offset.zero); //Get position
+                            final popupWidth = 200.0;
+                            final popupHeight = 300.0;
+
+                            final positionX = offset.dx;
+                            final positionY = offset.dy + renderBox.size.height;
+                            showMenu(
+                                context: context,
+                                position: RelativeRect.fromLTRB(
+                                  positionX, // x-position
+                                  positionY, // y-position
+                                  0, // width offset
+                                  0, // height offset
+                                ),
+                                items: getMonthNames('en_US').map((month) {
+                                  return PopupMenuItem<String>(
+                                      value: month, child: Text(month));
+                                }).toList());
+                          },
+                          child: Text(
+                            formattedMonth,
+                            style: widget.headerStyle.titleTextStyle,
+                            textAlign: widget.headerStyle.titleCentered
+                                ? TextAlign.center
+                                : TextAlign.start,
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          formattedYear,
+                          style: widget.headerStyle.titleTextStyle,
+                          textAlign: widget.headerStyle.titleCentered
+                              ? TextAlign.center
+                              : TextAlign.start,
+                        ),
+                        SizedBox(width: 10),
+                      ],
+                    ),
+                  ),
             ),
-          if (headerStyle.rightChevronVisible)
-            CustomIconButton(
-              icon: headerStyle.rightChevronIcon,
-              onTap: onRightChevronTap,
-              margin: headerStyle.rightChevronMargin,
-              padding: headerStyle.rightChevronPadding,
-            ),
-        ],
+            if (widget.headerStyle.formatButtonVisible &&
+                widget.availableCalendarFormats.length > 1)
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: FormatButton(
+                  onTap: widget.onFormatButtonTap,
+                  availableCalendarFormats: widget.availableCalendarFormats,
+                  calendarFormat: widget.calendarFormat,
+                  decoration: widget.headerStyle.formatButtonDecoration,
+                  padding: widget.headerStyle.formatButtonPadding,
+                  textStyle: widget.headerStyle.formatButtonTextStyle,
+                  showsNextFormat: widget.headerStyle.formatButtonShowsNext,
+                ),
+              ),
+            if (widget.headerStyle.rightChevronVisible)
+              CustomIconButton(
+                icon: widget.headerStyle.rightChevronIcon,
+                onTap: widget.onRightChevronTap,
+                margin: widget.headerStyle.rightChevronMargin,
+                padding: widget.headerStyle.rightChevronPadding,
+              ),
+          ],
+        ),
       ),
     );
   }
